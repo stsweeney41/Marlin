a064de3399510e63f64372c1d541215ae46422bb
stsweeney41 <38888209+stsweeney41@users.noreply.github.com>
2020-07-21 12:34:24 -0300

    Update esteps

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -693,7 +693,7 @@
 
 // Enable this feature if all enabled endstop pins are interrupt-capable.
 // This will remove the need to poll the interrupt pins, saving many CPU cycles.
-//#define ENDSTOP_INTERRUPTS_FEATURE
+#define ENDSTOP_INTERRUPTS_FEATURE
 
 /**
  * Endstop Noise Threshold
@@ -734,7 +734,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 499 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 431 }
 
 /**
  * Default Max Feed Rate (mm/s)

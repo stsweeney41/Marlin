d6ad56477af517fa55019862dc9e8167b7f12f9a
stsweeney41 <38888209+stsweeney41@users.noreply.github.com>
2020-08-23 19:10:09 -0300

    fix bltouch

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -832,7 +832,7 @@
  *
  * Enable this option for a probe connected to the Z Min endstop pin.
  */
-//#define Z_MIN_PROBE_USES_Z_MIN_ENDSTOP_PIN
+#define Z_MIN_PROBE_USES_Z_MIN_ENDSTOP_PIN
 
 /**
  * Z_MIN_PROBE_PIN
@@ -850,7 +850,7 @@
  *      - normally-open switches to 5V and D32.
  *
  */
-#define Z_MIN_PROBE_PIN P0_10 // Pin 32 is the RAMPS default
+//#define Z_MIN_PROBE_PIN P0_10 // Pin 32 is the RAMPS default
 
 /**
  * Probe Type
